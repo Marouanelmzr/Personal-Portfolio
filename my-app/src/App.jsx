7f21afb28f5523a938cd285c363a4b45f6f467d9
Marouanelmzr <elmozariahimarouane05@gmail.com>
2025-07-15 13:13:39 +0100

    about page

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -10,6 +10,23 @@ import { FaCircleArrowRight } from "react-icons/fa6";
 import { FaCircleArrowLeft } from "react-icons/fa6";
 import Paragraph from './assets/components/paragraph.jsx'
 import CursorDot from './assets/components/CursorDot.jsx';
+import marouanesketch from './assets/images/marouanesketch.png'
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiTypescript } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { SiPostgresql } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
+import { FaGitAlt } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { SiAdobephotoshop } from "react-icons/si";
+import { SiAdobeillustrator } from "react-icons/si";
+import { SiFigma } from "react-icons/si";
+import { SiBlender } from "react-icons/si";
+import { SiCinema4D } from "react-icons/si";
+import zbrush from './assets/images/zbrush.webp'
+import substancepainter from './assets/images/substance-painter.png'
+
 
 function App() {
 
@@ -172,7 +189,7 @@ useEffect(() => {
           <div className="white-circle"></div>
         </div>
         <div className='bio-container'>
-          <h4>Hi, I’m Marouane — a designer and aspiring software engineer blending creativity with code. <span class="block">  Passionate about building clean, functional experiences. Currently based in Tangier, Morocco.</span></h4>
+          <h4>Hi, I’m Marouane a designer and aspiring software engineer blending creativity with code. <span class="block">  Passionate about building clean, functional experiences. Currently based in Tangier, Morocco.</span></h4>
         </div>
     </section>
 
@@ -239,6 +256,95 @@ useEffect(() => {
           <FaCircleArrowRight className='slider-button' onClick={ handleNext } disabled={currentIndex === 4}/>
         </div>
       </div>
+      </div>
+    </section>
+    <section className='about-container' id='about'>
+      <h2 className='title-red'></h2>
+      <div className='about-wrapper'>
+        <img src={marouanesketch} alt="Marouane Sketch" className='marouane-sketch' />
+        <div className='about-right'>
+          <h1>HELLO</h1>
+          <h2>I'm <span className='bold'>Marouane Elmozariahi</span>,</h2>
+          <h3>a multidisciplinary creative with a passion for visual storytelling, interactive design, and digital experiences.</h3>
+          <h4>My journey began with an obsession for video games, movies, and posters. That fascination grew into a desire to create immersive worlds and visuals of my own. Over time, I explored everything from graphic design and 3D sculpting to web development, shaping a toolkit that merges art with technology. Whether it’s a line of code or a brushstroke, I treat every project as a chance to tell a story that feels human, thoughtful, and a little bit unexpected. I’m driven by curiosity, intentionality, and a constant hunger to learn something new.</h4>
+          <div className='about-stacks'>
+          <p className='smaller'>Stacks</p>
+          <h2>Code & Frameworks</h2>
+          <div className='stacks'>
+            <div className='stacks-scroll'>
+            <span className='stack'> <IoLogoJavascript className='icon-stack'/> Java Script</span>
+            <span className='stack'> <SiTypescript className='icon-stack'/> Type Script</span>
+            <span className='stack'> <FaReact className='icon-stack'/> React</span>
+            <span className='stack'> <SiPostgresql className='icon-stack'/> PostgreSQL</span>
+            <span className='stack'> <FaNodeJs className='icon-stack'/> Node.js</span>
+            <span className='stack'> <FaGitAlt className='icon-stack'/> Git</span>
+            <span className='stack'> <FaCss3Alt className='icon-stack'/> CSS</span>
+            <span className='stack'> <FaHtml5 className='icon-stack'/> HTML</span>
+
+            <span className='stack'> <IoLogoJavascript className='icon-stack'/> Java Script</span>
+            <span className='stack'> <SiTypescript className='icon-stack'/> Type Script</span>
+            <span className='stack'> <FaReact className='icon-stack'/> React</span>
+            <span className='stack'> <SiPostgresql className='icon-stack'/> PostgreSQL</span>
+            <span className='stack'> <FaNodeJs className='icon-stack'/> Node.js</span>
+            <span className='stack'> <FaGitAlt className='icon-stack'/> Git</span>
+            <span className='stack'> <FaCss3Alt className='icon-stack'/> CSS</span>
+            <span className='stack'> <FaHtml5 className='icon-stack'/> HTML</span>
+            </div>
+          </div>
+          <h2>Design Tools</h2>
+          <div className='stacks'>
+            <div className='stacks-scroll-reverse'>
+              <span className='stack'> <SiAdobephotoshop className='icon-stack'/> Photoshop</span>
+              <span className='stack'> <SiAdobeillustrator className='icon-stack'/> Illustrator</span>
+              <span className='stack'> <SiFigma className='icon-stack'/> Figma</span>
+              <span className='stack'> <SiBlender className='icon-stack'/> Blender</span>
+              <span className='stack'> <SiCinema4D className='icon-stack'/> Cinema 4D</span>
+              <span className='stack'> <img src={zbrush}className='icon-stack'/> ZBrush</span>
+              <span className='stack'> <img src={substancepainter} className='icon-stack'/> Substance Painter</span>
+
+              <span className='stack'> <SiAdobephotoshop className='icon-stack'/> Photoshop</span>
+              <span className='stack'> <SiAdobeillustrator className='icon-stack'/> Illustrator</span>
+              <span className='stack'> <SiFigma className='icon-stack'/> Figma</span>
+              <span className='stack'> <SiBlender className='icon-stack'/> Blender</span>
+              <span className='stack'> <SiCinema4D className='icon-stack'/> Cinema 4D</span>
+              <span className='stack'> <img src={zbrush}className='icon-stack'/> ZBrush</span>
+              <span className='stack'> <img src={substancepainter} className='icon-stack' fill="#363732"/> Substance Painter</span>
+
+
+            
+            </div>
+          </div>
+          </div>
+        <div className='about-experience-education'>
+          <div className='about-experience'>
+          <p>Experience</p>
+          <h2>XELOPS Technology</h2>
+          <h3>Remote Internship - June 2025</h3>
+          <h4>Co-developed an absence management platform with one teammate: UI design, frontend/backend coding, and SQL database setup.</h4>
+          <h2>PandoraBox Communication</h2>
+          <h3>Internship - July 2024</h3>
+          <h4>Designed visual assets for high-profile clients such as Orange, Renault, and Tanger City Mall.</h4>
+          <h2>Freelance Digital Designer</h2>
+          <h3>Self-Employed - 2021/2024</h3>
+          <h4>Delivered posters, logos, and 3D visuals for clients such as Juggernaut Energy, esports organizations, and various independent creatives and businesses.</h4>
+          </div>
+          <div className='about-education'>
+          <p>Education</p>
+          <h2>ENSA, Tangier</h2>
+          <h3>2024 - 2028</h3>
+          <h4>Master's degree in Computer Science.</h4>
+          <p>Soft skills</p>
+          <span className='soft-skills'>
+          <h4>Multilingual Communication (Fluent in Arabic, French, and English)</h4>
+          <h4>Adaptability & Flexibility</h4>
+          <h4>Teamwork & Collaboration</h4>
+          <h4>Creative Problem Solving</h4>
+          <h4>Time Management</h4>
+          <h4>Open to Feedback</h4>
+          </span>
+          </div>
+        </div>
+        </div>
       </div>
     </section>
     </div>
